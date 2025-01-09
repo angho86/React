435f4ff7b7603a86b3dc6e6dc0d01a2ed1674179
EnjoyDS <102280556+EnjoyDS@users.noreply.github.com>
2025-01-09 20:57:58 +0200

    added registration function

--- Project Management/back-end/controllers/auth-controller.js
+++ Project Management/back-end/controllers/auth-controller.js
@@ -8,5 +8,59 @@ import { Op } from "sequelize";
 
 export async function register(req, res) {
     // patikrinimas aar naudotojas prisijunges
-    
+    if (req.session.isLogged) {
+        return res.status(403).json({
+            message: "You are already logged in. Please log out to create a new user",
+        });
+    }
+
+    // duomenu is kliento gavimas
+
+    const { username, email, password } = req.body;
+
+    try {
+        // validaciju vykdymas
+
+        const validationResult = registrationSchema.safeParse(req.body);
+
+        if(!validationResult.success){
+            return res.status(400).json({ message: validationResult.error.issues });
+        }
+
+        // hashuojame slaptazodi
+
+        const salt = generateSalt();
+        const hashedPassword = hashPassword(password, salt);
+
+        // userio kurimas duomenu bazeje
+
+        const user = await UserModel.create({
+            username,
+            email,
+            hashedPassword,
+            salt,
+        });
+
+        // sesijos duomenu sudarymas
+
+        req.session.user = {
+            email,
+            username,
+        };
+        req.session.isLogged = true;
+
+        // registracija sekminga, atiduodame sekminga atsakyma
+
+        res.status(201).json({ message: "Registration was successful", session: req.session });
+
+    }  catch (err) {
+        if (err?.original && err.original.errno === 1062){
+            return res.status(400).json({ message: "Username or Email field was not unique "});
+        }
+
+        res.status(500).json({ message: "internal server error", err: err.message });
+    }
 }
+
+
+
